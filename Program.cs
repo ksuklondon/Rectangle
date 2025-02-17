9d77d70f3b429e03d948a8a89758a057cf47cba4
stani <stani@MYYOGA>
2025-02-17 21:53:09 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prostokat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostokat mojProstokat = new Prostokat(4, 4);
+
+            Console.WriteLine($"Szerokość: {mojProstokat.Szerokosc} cm");
+            Console.WriteLine($"Wysokość: {mojProstokat.Wysokosc} cm");
+
+            Console.WriteLine($"Pole: {mojProstokat.ObliczPole():0.00} cm²");
+            Console.WriteLine($"Obwód: {mojProstokat.ObliczObwod():0.00} cm");
+
+            mojProstokat.Rysuj();
+        }
+    }
+}
---
+++ Prostokat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prostokat
+{
+    public class Prostokat
+    {
+        // Wlasciwosci
+        public double szerokosc;
+        public double wysokosc;
+
+        // Konstruktor parametryczny
+        public Prostokat(double szerokosc, double wysokosc)
+        {
+            this.szerokosc = szerokosc;
+            this.wysokosc = wysokosc;
+        }
+
+        public double Szerokosc
+        {
+            get { return szerokosc; }
+            set { if (value > 0) szerokosc = value; }
+        }
+
+        public double Wysokosc
+        {
+            get { return wysokosc; }
+            set { if (value > 0) wysokosc = value; }
+        }
+
+
+        // Metody
+        public double ObliczPole()
+        {
+            return szerokosc * wysokosc;
+        }
+
+        public double ObliczObwod()
+        {
+            return 2 * (szerokosc + wysokosc);
+        }
+
+        public void Rysuj()
+        {
+            for (int i = 0; i < wysokosc; i++)
+            {
+                for (int j = 0; j < szerokosc; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
